eb832712ee8824e14d41e878af8401178e87a4ab
Arthur <arthur.bouvet35@gmail.com>
2025-05-14 11:13:19 +0200

    MAJ automatique de l'objectif, tp2

--- tp_rob201/my_robot_slam.py
+++ tp_rob201/my_robot_slam.py
@@ -35,17 +35,22 @@ class MyRobotSlam(RobotAbstract):
         # robot's starting position and the maximum map size that we shouldn't know.
         size_area = (1400, 1000)
         robot_position = (100.0, 100)
-        self.occupancy_grid = OccupancyGrid(x_min=-(size_area[0] / 2 + robot_position[0]),
-                                            x_max=size_area[0] / 2 - robot_position[0],
-                                            y_min=-(size_area[1] / 2 + robot_position[1]),
-                                            y_max=size_area[1] / 2 - robot_position[1],
-                                            resolution=2)
+        x_min=-(size_area[0] / 2 + robot_position[0])
+        x_max=size_area[0] / 2 - robot_position[0]
+        y_min=-(size_area[1] / 2 + robot_position[1])
+        y_max=size_area[1] / 2 - robot_position[1]
+        self.occupancy_grid = OccupancyGrid(x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max, resolution=2)
 
         self.tiny_slam = TinySlam(self.occupancy_grid)
         self.planner = Planner(self.occupancy_grid)
 
         # storage for pose after localization
         self.corrected_pose = np.array([0, 0, 0])
+
+        #initialization of the objective
+        self.goal_pose = np.array([50.0, 50.0, 0.0])  #initial objective
+        self.goal_reached_threshold = 30.0  #threshold to consider the goal reached
+
 
     def control(self):
         """
@@ -70,7 +75,25 @@ class MyRobotSlam(RobotAbstract):
         Main control function with full SLAM, random exploration and path planning
         """
         pose = self.odometer_values()
-        goal = [400,0,0]
+        goal = self.goal_pose
+        dist_to_goal = np.linalg.norm(goal[:2] - pose[:2])
+
+        if (dist_to_goal<self.goal_reached_threshold):
+            print("Goal reached.")
+
+            size_area = (1400, 1000)
+            robot_position = (100.0, 100)
+            
+            x_min=-(size_area[0] / 2 + robot_position[0])
+            x_max=size_area[0] / 2 - robot_position[0]
+            y_min=-(size_area[1] / 2 + robot_position[1])
+            y_max=size_area[1] / 2 - robot_position[1]
+
+            new_x = np.random.uniform(x_min+100, x_max-100)
+            new_y = np.random.uniform(y_min+100, y_max-100)
+
+            self.goal_pose = np.array([new_x, new_y, 0.0])
+            print("New goal: ", self.goal_pose)
 
         # Compute new command speed to perform obstacle avoidance
         command = potential_field_control(self.lidar(), pose, goal)
